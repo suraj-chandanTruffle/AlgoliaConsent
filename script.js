e7a766c56f5a15f203c9f2108df339de684bb753
suraj-chandanTruffle <suraj.chandan@trufflecorp.com>
2025-12-12 20:19:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log('Hello World!');
 window.addEventListener("message", (event) => {
   if (event.data && event.data.type === "pref-center:update") {
     console.log("Preference Center updated:", event.data);
