34475fd9e906cce80ebde2d9d9fe6ac387975bc7
suraj-chandanTruffle <suraj.chandan@trufflecorp.com>
2025-12-15 19:11:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,15 @@
-console.log('Hello World!');
 let emailValue = '';
 document.addEventListener("change", function (e) {
     emailValue = e.detail.value;
-    console.log("Preference Center updated:", emailValue);
 });
+function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
 window.addEventListener("click", function (e) {
-    console.log("Preference Center updated:", emailValue);
-    const realButton = event.composedPath().find(el => el.tagName === 'BUTTON');
-    if(realButton && realButton.innerHTML == 'Unsubscribe'){
-        window.location.href = 'https://go.algolia.com/unsubscribeconfirmation.html';
-        window.location.replace('https://go.algolia.com/unsubscribeconfirmation.html');
-        //window.location.reload();
-        //window.open('https://go.algolia.com/unsubscribeconfirmation.html', '_blank');
+    if(emailValue && isValidEmail(emailValue)){
+        const realButton = event.composedPath().find(el => el.tagName === 'BUTTON');
+        if(realButton && realButton.innerHTML == 'Unsubscribe'){
+            window.location.href = 'https://go.algolia.com/unsubscribeconfirmation.html';
+        }
     }
 });
-/*document.addEventListener("commit", () => {
-    console.log("Preference Center commited:");
-});
-window.addEventListener("commit", () => {
-    console.log("Preference Center commited:");
-});
-window.addEventListener("select", function (e) {
-    console.log("Preference Center selected:", e);
-});
-window.addEventListener("privatebuttonregister", function (e) {
-    console.log("Preference Center privatebuttonregistered:", e);
-});
-window.addEventListener("GET_LINK_INFO_EVENT", function (e) {
-    console.log("Preference Center GET_LINK_INFO_EVENT:", e);
-});
-window.addEventListener("formfieldchange", function (e) {
-    console.log("Preference Center formfieldchange:", e);
-});
-window.addEventListener("endreached", function (e) {
-    console.log("Preference Center endreached:", e);
-});
-window.addEventListener("focus", (e) => {
-    console.log("Preference Center focus:");
-});
-window.addEventListener("blur", (e) => {
-    console.log("Preference Center blur:");
-});
-window.addEventListener("textchange", function (e) {
-    console.log("Preference Center textchange:", e);
-});
-window.addEventListener("invalid", function (e) {
-    console.log("Preference Center invalid:", e);
-});
-window.addEventListener("open", (e) => {
-    console.log("Preference Center open:");
-});
-window.addEventListener("emailTextInput", function (e) {
-    console.log("Preference Center emailTextInput:");
-});
-window.addEventListener("textinput", function (e) {
-    console.log("Preference Center textinput:", e);
-});
-window.addEventListener("input", function (e) {
-    console.log("Preference Center textinput:", e);
-});
-window.addEventListener("clickEvent", function (e) {
-    console.log("Preference Center clickEvent:", e);
-});*/
-
-
